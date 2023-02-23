3beaced9c3543beee9f3548af54b1fc8e49aec04
Amaluvs <126064533+Amaluvs@users.noreply.github.com>
2023-02-23 12:08:18 +0530

    Add files via upload
    Addition of two numbers

---
+++ AddTwo.java
@@ -0,0 +1,21 @@
+class AddTwo
+{
+void clap()
+{
+	int x=2,y=4,y=6,d=0;
+ d=a+b+c/3;
+	System.out.print("sum of 3 number is"+d);
+}
+void clap()
+{
+	float x=4.0,y=5.0,z=6.0,k=0;
+	 k=a+b+c/3;
+	System.out.print("sum of 3 float number is"+k);
+}
+public static void main(String args[])
+{
+	AddTwo.obj=new AddTwo();
+	obj.clap();
+	obj.clap();
+}
+}
